f3585e75e9fe4a584a42e7dbc5cdc8c9bfa8dc8e
l <aksahy.koulu@example.com>
2021-03-31 01:26:37 +0200

    const-fn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 //dont need the .js files 
-import countries from './objects/objLiteral'
+import countries from './objects/const-fn'
 
 function App() {
 
 // console.log(countries)
 // deconstruction 
  const [be,usa,ind ] = countries
+ 
  //const [b,...rest ] = countries
  //console.log(rest)
+
+   //PROBLEM: 
+   console.dir(be)
+   console.dir(usa)
+   console.dir(ind)
 
   return (
   <div className="container-fluid">
@@ -25,21 +31,21 @@ function App() {
     <div className="text">{be.name}</div>
     <div className="text">{be.dosesGiven}</div>
     <div className="text">{be.vaccinated}</div>
-    <div className="text">  -  </div>    
+    <div className="text">{be.percentage()} </div>    
   </div>
 
   <div className="table-row">
     <div className="text">{usa.name}</div>
     <div className="text">{usa.dosesGiven}</div>
     <div className="text">{usa.vaccinated}</div>    
-    <div className="text"> - </div>    
+    <div className="text">{usa.percentage()} </div>    
   </div>
 
   <div className="table-row">
     <div className="text">{ind.name}</div>
     <div className="text">{ind.dosesGiven}</div>
     <div className="text">{ind.vaccinated}</div>    
-    <div className="text"> - </div>    
+    <div className="text">{ind.percentage()} </div>    
   </div>
 
 </div>
---
+++ src/objects/const-fn.js
@@ -0,0 +1,18 @@
+
+// convention, the Constructor function start with C 
+const Country = function(name,dosesGiven,vaccinated, population){
+    this.name = name;
+    this.dosesGiven= dosesGiven;
+    this.vaccinated = vaccinated;
+    this.population = population;
+    this.percentage = function(){
+        return    ((this.vaccinated /this.population)*100).toFixed(2)
+    }
+}
+
+// the new keyword... creates a object,
+const belgium = new Country("Belgium","1.74M","50600",500000) 
+const usa = new Country("United States","20M","500600",1500000) 
+const india = new Country("India","200M","5220600",522200000) 
+
+export default [belgium,usa,india]
